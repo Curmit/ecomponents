67f94d9e38b24b1388ce84f1048ef7014f7c4c41
Carmen <c.n.burghardt@student.utwente.nl>
2016-07-07 15:55:30 +0000

    this is the development branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: "this is the development branch "
+  end
+  
+  
+  
 end
