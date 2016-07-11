b07a5121570d601150cdefa6b4d6189b17bf85e6
Carmen <c.n.burghardt@student.utwente.nl>
2016-07-11 16:25:17 +0000

    changed read me file and gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "this is the development branch "
+    render html: "this is the development branch .....  "
   end
   
   
